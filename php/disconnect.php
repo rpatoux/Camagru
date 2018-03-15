d3b151a65fc5a0ccd35c3dc4ad8193005ee45847
Robin PATOUX <rpatoux@e1r4p9.42.fr>
2018-03-15 19:21:34 +0100

    :

--- php/disconnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$url = $_SESSION['url'];
-	session_destroy();
-	header('Location:../accueil.php');
-?>
--- php/send_mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-function send_mail($mail, $message, $subject)
-{
-	$to = $mail;
-	$headers = 'From: admin@camagru.com' . "\r\n" .'X-Mailer: PHP/' . phpversion();
-	mail($to, $subject, $message, $headers);
-}
-?>
